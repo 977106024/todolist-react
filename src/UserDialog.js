5b216dec2b5b9dddf89e2b34b784436c386ebcf9
977106024 <977106024@qq.com>
2017-11-11 20:54:33 +0800

    登陆注册框

---
+++ src/UserDialog.js
@@ -0,0 +1,56 @@
+import React,{Component} from 'react'
+import './UserDialog.css'
+
+export default class UserDialog extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      selected: 'signUp'
+    }
+  }
+  switch(e){
+    this.setState({
+      selected: e.target.value
+    })
+  }
+  render(){
+    return(
+      <div className="UserDialog">
+        <div className="UserDialog">
+          <nav onChange={this.switch.bind(this)}>
+            <input type="radio" value="signUp" checked={this.state.selected === 'signUp'}/> 注册
+            <input type="radio" value="signIn" checked={this.state.selected === 'signIn'}/> 登陆
+          </nav>
+          <div className="panes">
+            <form className="signUp">
+              <div className="row">
+                <label>用户名</label>
+                <input type="text"/>
+              </div>
+              <div className="row">
+                <label>密码</label>
+                <input type="text"/>
+              </div>
+              <div className="row actions">
+                <button type="submit">注册</button>
+              </div>
+            </form>
+            <form className="signUp">
+              <div className="row">
+                <label>用户名</label>
+                <input type="text"/>
+              </div>
+              <div className="row">
+                <label>密码</label>
+                <input type="password"/>
+              </div>
+              <div className="row actions">
+                <button type="submit">登陆</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'a0RRL8DFj0fxYeB34yVQVfRW-gzGzoHsz'
+var APP_KEY = 'NUSTj9GhLqpQcPzcLACmVnfc'
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+export default AV
